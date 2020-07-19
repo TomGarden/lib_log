6d19287799739549bb7c56d7982ff54cd507d11a
Tom <01wukong10@gmail.com>
2020-07-19 16:10:34 +0800

    发包 v0.1.12

--- LibLog/bintrayUpload.sh
+++ LibLog/bintrayUpload.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # 关于 ： 语法 是在 PermissionDispatcher 学的，这个语法还是可以继续研究一下的
-../gradlew clean
-../gradlew build
-../gradlew publish
+../gradlew :LibLog:clean
+../gradlew :LibLog:build
+../gradlew :LibLog:publish
 
 #pwd
